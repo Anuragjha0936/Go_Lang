4a93bf9b5a47914f4f8b9cba2999e1c0c7f9888d
Anurag Shekhar Jha <anuragjha4032@gmail.com>
2026-09-20 19:53:57 +0530

    Update main.go

--- chat_app/main.go
+++ chat_app/main.go
@@ -4,24 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	
+	"os"
+
 	"github.com/joho/godotenv"
 	"main.go/WS"
 	"main.go/router"
 )
 
-func init(){
-	err:=godotenv.Load()
-	if err!=nil{
-		log.Println(".env file not found, using Railway environment variables")
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found, using environment variables")
 	}
 }
-func main()  {
-	hub:=WS.NewHub()
-	r:=router.SetUpRoute(hub)
-	fmt.Println("Server is Running")
-	err:=http.ListenAndServe(":8080",r)
-	if err!=nil{
+
+func main() {
+	hub := WS.NewHub()
+	r := router.SetUpRoute(hub)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "10000"
+	}
+
+	fmt.Println("Server is Running on port:", port)
+
+	err := http.ListenAndServe("0.0.0.0:"+port, r)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
